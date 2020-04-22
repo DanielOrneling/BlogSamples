b7eec631b89941861cc047eca8516fd70f1eb98f
Daniel Örneling <orneling1987@hotmail.com>
2020-04-22 08:34:42 +0200

    New files

---
+++ TagsSample/Deploy.ps1
@@ -0,0 +1,21 @@
+# Connect to Azure
+Connect-AzAccount
+
+# Set the values to use for the deployment
+$DeploymentName = "ConcatenateBlogSample"
+$rgName = "ConcatenateBlogSample"
+$location = "westeurope"
+$Subscription = "YourSubscriptionNameHere"
+
+# Set the AZ Subscription to work against
+Get-AzSubscription -SubscriptionName $Subscription | Set-AzContext
+
+# Deploy the resource group
+New-AzResourceGroup -Name $rgName -Location $location
+
+# Deploy the resources from the ARM template
+New-AzResourceGroupDeployment `
+-Name $DeploymentName `
+-ResourceGroupName $rgName `
+-TemplateFile .\azuredeploy.json `
+-TemplateParameterFile .\azuredeploy.parameters.json
