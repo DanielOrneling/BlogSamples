9781de7255e6c423cb1a5c0743681509d4b4b97a
Daniel Örneling <orneling1987@hotmail.com>
2020-05-19 18:50:30 +0200

    New addition for new blog post

---
+++ NestedTemplateSample/Deploy.ps1
@@ -0,0 +1,22 @@
+# Connect to Azure
+Connect-AzAccount
+
+
+# Set the values to use for the deployment
+$DeploymentName = "AzureMonitorDeployment1337"
+$rgName = "AzureMonitor1337"
+$location = "westeurope"
+$Subscription = "AzureMSDN"
+
+# Set the AZ Subscription to work against
+Get-AzSubscription -SubscriptionName $Subscription | Set-AzContext
+
+# Deploy the resource group
+New-AzResourceGroup -Name $rgName -Location $location
+
+# Deploy the resources from the ARM template
+New-AzResourceGroupDeployment `
+-Name $DeploymentName `
+-ResourceGroupName $rgName `
+-TemplateFile .\azuredeploy.json `
+-TemplateParameterFile .\azuredeploy.parameters.json
